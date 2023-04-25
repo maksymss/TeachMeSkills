eebb2d500815f3d9d07d8416cb4c53d5545a8357
maksymss <maksym.sudarikovnyc@gmail.com>
2023-04-25 13:27:46 -0400

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Initialized class!
+public class Main {
+    //Initialized main method!
+    public static void main(String[] args) {
+        //Printed "Hello World!" string to the console
+        System.out.println("Hello World!");
+
+    }
+}
