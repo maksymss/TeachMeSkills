17ca93fb130f02aab2c19e64355a8b13029410f6
maksymss <maksym.sudarikovnyc@gmail.com>
2023-04-26 14:48:21 -0400

    Grid & Christmas tree

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 //Initialized class!
 public class Main {
     //Initialized main method!
     public static void main(String[] args) {
-        //Printed "Hello World!" string to the console
+        /*Printed "Hello World!" string to the console
         //System.out.println("Hello World!");
         //Scanner userInput = new Scanner(System.in);
         //System.out.println("Enter username");
 
         //String userName = userInput.nextLine();
-        //System.out.println("Username is: " + userName);
+        System.out.println("Username is: " + userName);*/
 
         System.out.println("    |    |    ");
         System.out.println("____|____|____");
