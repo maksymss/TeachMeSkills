6e26d82b7163f3fd374b8c468d03c41858fcb2ad
maksymss <maksym.sudarikovnyc@gmail.com>
2023-05-01 12:24:35 -0400

    updated solution using for loop

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,14 @@ public class Main {
         System.out.println(" ******* ");
         System.out.println("*********");
 
+        //for loop option
+        //Tried to do it myself, but ended up googling the solution
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+
     }
 }
